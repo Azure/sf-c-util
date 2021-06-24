ef34e4cc78a7719f192a305a5646c7038060e971
Dan Cristoloveanu <dcristo@microsoft.com>
2021-06-23 19:27:01 -0700

    Fix cmakelists for sf_c_util_reals

--- tests/reals/real_hresult_to_string_renames.h
+++ tests/reals/real_hresult_to_string_renames.h
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #define hresult_to_string                                        real_hresult_to_string
 #define hresult_to_wstring                                       real_hresult_to_wstring
