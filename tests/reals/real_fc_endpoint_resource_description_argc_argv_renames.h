b6190bce9e8d06a95ae229bb3fa4f36f59ffa3ff
Andrei Porumb <anporumb@microsoft.com>
2022-11-02 11:52:07 -0700

    added reals for endpoint_resource_description

---
+++ tests/reals/real_fc_endpoint_resource_description_argc_argv_renames.h
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_to_ARGC_ARGV           real_FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_to_ARGC_ARGV
+#define FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_from_ARGC_ARGV         real_FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_from_ARGC_ARGV
+#define FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_free                   real_FABRIC_ENDPOINT_RESOURCE_DESCRIPTION_free
+
