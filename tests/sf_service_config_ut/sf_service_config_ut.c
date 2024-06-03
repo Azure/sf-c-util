ff687605afced38ae39eda07179b3c0dd3eeb65e
Dan Cristoloveanu <dcristo@microsoft.com>
2024-06-03 12:54:30 -0700

    Address CR comment

--- tests/sf_service_config_ut/sf_service_config_ut.c
+++ tests/sf_service_config_ut/sf_service_config_ut.c
@@ -76,8 +76,16 @@ TEST_SF_SERVICE_CONFIG_DEFINE_EXPECTED_CALL_HELPERS(my_config, expected_config_p
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_string_option_optional L"OptionalStringOption"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_wide_string_option_optional L"OptionalWideStringOption"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_flag L"AnotherFlag"
-#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_secret L"ASecret"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_a_secret L"ASecret"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_a_secret_uint8_t L"ASecretUInt8"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_a_secret_char_ptr L"ASecretCharPtr"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_a_secret_double L"ASecretDouble"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_a_secret_bool L"ASecretBool"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_secret L"AnotherSecret"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_secret_uint8_t L"AnotherSecretUInt8"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_secret_char_ptr L"AnotherSecretCharPtr"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_secret_double L"AnotherSecretDouble"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_mocked_another_secret_bool L"AnotherSecretBool"
 
 #define MY_MOCKED_CONFIG_TEST_PARAMS \
     CONFIG_REQUIRED(uint64_t, mocked_parameter_1), \
@@ -94,7 +102,15 @@ TEST_SF_SERVICE_CONFIG_DEFINE_EXPECTED_CALL_HELPERS(my_config, expected_config_p
     CONFIG_OPTIONAL(wchar_ptr, mocked_wide_string_option_optional), \
     CONFIG_REQUIRED(bool, mocked_another_flag), \
     CONFIG_REQUIRED_NO_LOGGING(thandle_rc_string, a_secret), \
-    CONFIG_OPTIONAL_NO_LOGGING(char_ptr, another_secret) \
+    CONFIG_REQUIRED_NO_LOGGING(uint8_t, a_secret_uint8_t), \
+    CONFIG_REQUIRED_NO_LOGGING(char_ptr, a_secret_char_ptr), \
+    CONFIG_REQUIRED_NO_LOGGING(double, a_secret_double), \
+    CONFIG_REQUIRED_NO_LOGGING(bool, a_secret_bool), \
+    CONFIG_OPTIONAL_NO_LOGGING(thandle_rc_string, another_secret), \
+    CONFIG_OPTIONAL_NO_LOGGING(uint8_t, another_secret_uint8_t), \
+    CONFIG_OPTIONAL_NO_LOGGING(char_ptr, another_secret_char_ptr), \
+    CONFIG_OPTIONAL_NO_LOGGING(double, another_secret_double), \
+    CONFIG_OPTIONAL_NO_LOGGING(bool, another_secret_bool) \
 
 DECLARE_SF_SERVICE_CONFIG(my_mocked_config, MY_MOCKED_CONFIG_TEST_PARAMS)
 
--- tests/sf_service_config_ut/test_sf_service_config.h
+++ tests/sf_service_config_ut/test_sf_service_config.h
@@ -27,7 +27,15 @@
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_wide_string_option_optional L"OptionalWideStringOption"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_another_flag L"AnotherFlag"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_a_secret L"ASecret"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_a_secret_uint8_t L"ASecretUint8t"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_a_secret_char_ptr L"ASecretCharPtr"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_a_secret_double L"ASecretDouble"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_a_secret_bool L"ASecretBool"
 #define SF_SERVICE_CONFIG_PARAMETER_NAME_another_secret L"AnotherSecret"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_another_secret_uint8_t L"AnotherSecretUint8t"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_another_secret_char_ptr L"AnotherSecretCharPtr"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_another_secret_double L"AnotherSecretDouble"
+#define SF_SERVICE_CONFIG_PARAMETER_NAME_another_secret_bool L"AnotherSecretBool"
 
 #define MY_CONFIG_TEST_PARAMS \
     CONFIG_REQUIRED(uint64_t, parameter_1), \
@@ -44,7 +52,15 @@
     CONFIG_OPTIONAL(wchar_ptr, wide_string_option_optional), \
     CONFIG_REQUIRED(bool, another_flag), \
     CONFIG_REQUIRED_NO_LOGGING(thandle_rc_string, a_secret), \
-    CONFIG_OPTIONAL_NO_LOGGING(char_ptr, another_secret) \
+    CONFIG_REQUIRED_NO_LOGGING(uint8_t, a_secret_uint8_t), \
+    CONFIG_REQUIRED_NO_LOGGING(char_ptr, a_secret_char_ptr), \
+    CONFIG_REQUIRED_NO_LOGGING(double, a_secret_double), \
+    CONFIG_REQUIRED_NO_LOGGING(bool, a_secret_bool), \
+    CONFIG_OPTIONAL_NO_LOGGING(thandle_rc_string, another_secret), \
+    CONFIG_OPTIONAL_NO_LOGGING(uint8_t, another_secret_uint8_t), \
+    CONFIG_OPTIONAL_NO_LOGGING(char_ptr, another_secret_char_ptr), \
+    CONFIG_OPTIONAL_NO_LOGGING(double, another_secret_double), \
+    CONFIG_OPTIONAL_NO_LOGGING(bool, another_secret_bool) \
 
 DECLARE_SF_SERVICE_CONFIG(my_config, MY_CONFIG_TEST_PARAMS)
 
